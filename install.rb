5e5a68ed8ba2565c615793eff346882f3a7aaba1
Dave Giunta <dgiunta@mac.com>
2009-11-05 15:46:02 -0600

    [master] Updating install script to tell the user what it's doing.

--- install.rb
+++ install.rb
@@ -1,3 +1,7 @@
 #!/usr/bin/env ruby -wKU
 
+print "Copying command to /usr/local/bin: "
 system("sudo cp #{File.join(File.dirname(__FILE__), 'git-commit-with-branch')} /usr/local/bin")
+puts "Done"
+
+exit 0
